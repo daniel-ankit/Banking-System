5525bc1a0cb6bd400db3242a3b4a9e7952459e13
Ankit Kumar <ankit.kumar.061000@gmail.com>
2023-09-04 18:21:29 +0530

    Saving Database to Local Storage

--- Banking.cpp
+++ Banking.cpp
@@ -1,4 +1,5 @@
 #include<iomanip>
+#include <fstream>
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -31,7 +32,7 @@ class details : public Details {
 };
 
 class SavingAccount{
-	private:
+	protected:
 		details about;
 		int accountNum;
 		double balance;
@@ -40,7 +41,7 @@ class SavingAccount{
 		SavingAccount(string _firstName, string _lastName, int _age, string _phone, string _address, string _adhaarNumber, int _accountNum, double _balance = 0){
 			about = details(_firstName, _lastName, _age, _phone, _address, _adhaarNumber);
 			accountNum = _accountNum;
-			balance = _balance;
+			balance = _balance-0.1;
 		}
 		void print() {
 			cout << char(186) << " " << accountNum << " | "
@@ -57,14 +58,14 @@ class SavingAccount{
 		}
 		void deposit(double amount){
 			balance += amount;
-			cout << endl << "\x1B[92m TRANSCATION SUCESSFULL : AMOUNT CREDITED SUCESSFULLY. \033[0m" << endl;
+			cout << endl << "\x1B[92m TRANSACTION SUCESSFULL : AMOUNT CREDITED SUCESSFULLY. \033[0m" << endl;
 			cout << " CURRENT BALANCE : " << balance << endl << endl;
 		}
 		void withdraw(double amount){
 			if(balance >= amount)
 			{
 				balance = balance - amount;
-				cout << endl << "\x1B[92m TRANSCATION SUCESSFULL : MONEY HAS BEEN DEBITED FROM YOUR ACCOUNT AND DISBURSED AS CASH! \033[0m" << endl;
+				cout << endl << "\x1B[92m TRANSACTION SUCESSFULL : MONEY HAS BEEN DEBITED FROM YOUR ACCOUNT AND DISBURSED AS CASH! \033[0m" << endl;
 				cout << " CURRENT BALANCE : " << balance << endl << endl;
 			}
 			else
@@ -81,13 +82,36 @@ class currentAccount : public SavingAccount
 	double loanAmount = 0;
 	float rate = 3.25;
 	public:
+	currentAccount(string _firstName, string _lastName, int _age, string _phone, string _address, string _adhaarNumber, int _accountNum, double _balance, double _loan) : SavingAccount(_firstName, _lastName, _age, _phone, _address, _adhaarNumber, _accountNum, _balance){
+		loanAmount = _loan-0.1;
+	}
+	void print()
+	{
+		cout << char(186) << " " << accountNum << " | "
+			<< about.firstName << setw(15-(int)about.firstName.size()) << " | "
+			<< about.lastName << setw(15-(int)about.lastName.size()) << " | "
+			<< about.age << " | "
+			<< about.phone << " | "
+			<< about.address << setw(30-(int)about.address.size()) << " | "
+			<< about.adhaarNumber << " | "
+			<< balance << " | " << loanAmount << " " << char(186) << endl;
+	}
 	void loan(double amount)
 	{
 		loanAmount += amount;
+		cout << endl << "\x1B[92m TRANSACTION SUCESSFULL : YOU HAVE TAKEN A LOAN AGAINST YOUR CURRENT ACCOUNT. \033[0m" << endl;
+		cout << " CURRENT LOAN AMOUNT : " << loanAmount << endl << endl;
 	}
 	void payInstallment(double amount)
 	{
+		cout << endl << "\x1B[92m TRANSACTION SUCESSFULL : YOU HAVE PAID AN INSTALLMENT FOR YOUR LOAN. \033[0m" << endl;
+		cout << " CURRENT BALANCE : " << loanAmount << endl << endl;
 		loanAmount -= amount;
+	}
+	void addIntrest()
+	{
+		cout << "INTREST OF THIS MONTH IS BEEN ADDED TO THE LOAN AMOUNT" << endl;
+		loanAmount = loanAmount + (loanAmount*rate);
 	}
 };
 
@@ -111,7 +135,13 @@ class BankManagement{
 			}
 			else
 			{
-
+				cout << char(201);
+				for(int i=0; i<113; i++) cout << char(205);
+				cout << char(187) << endl;
+				cout << (char)186;
+				cout << " ACC NO |  FIRST NAME  |  LAST NAME   | AG |   PHONE   |           ADDRESS           | AADHAAR  |   BAL  | LOAN  ";
+				cout << char(186) << endl;
+				printLine(0);
 			}
 		}
 		void printLine(bool saving)
@@ -125,7 +155,9 @@ class BankManagement{
 			}
 			else
 			{
-				
+				cout << (char)199;
+				for(int i=0; i<113; i++) cout << char(196);
+				cout << char(182) << endl;
 			}
 		}
 		void printFooter(bool saving)
@@ -138,18 +170,21 @@ class BankManagement{
 			}
 			else
 			{
-
+				cout << char(200);
+				for(int i=0; i<113; i++) cout << char(205);
+				cout << char(188) << endl;
 			}
 		}
 
 	public:
-		void printInterface()
-		{
-
-		} 
-		void AddAccount(string _firstName, string _lastName, int _age, string _phone, string _address, string _adhaarNumber, int _accountNum, double _balance){
+		void AddSavingAccount(string _firstName, string _lastName, int _age, string _phone, string _address, string _adhaarNumber, int _accountNum, double _balance){
 			accounts.push_back(SavingAccount(_firstName, _lastName, _age, _phone, _address, _adhaarNumber, _accountNum, _balance));
 			turnover += _balance;
+			assets += _balance;
+		}
+		void AddCurrentAccount(string _firstName, string _lastName, int _age, string _phone, string _address, string _adhaarNumber, int _accountNum, double _balance, double _loan){
+			currentAccounts.push_back(currentAccount(_firstName, _lastName, _age, _phone, _address, _adhaarNumber, _accountNum, _balance, _loan));
+			turnover += _balance+_loan;
 			assets += _balance;
 		}
 		double printTurnover() {
@@ -169,6 +204,7 @@ class BankManagement{
 			}
 			printFooter(1);
 			cout << endl << "DETAILS OF CURREN ACCOUNTS THAT EXISTS IN OUR DATABASE : " << endl;
+			printHeaders(0);
 			for(int i=0; i<currentAccounts.size(); i++)
 			{
 				currentAccounts[i].print();
@@ -197,16 +233,37 @@ class BankManagement{
 			}
 			return NULL;
 		}
+		void searchCurrentAccount(int accountNum){
+			auto find = findCurrentAccount(accountNum);
+			if(find)
+			{
+				printHeaders(0);
+				find->print();
+				printFooter(0);
+			}
+			else cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
+		}
+		currentAccount* findCurrentAccount(int accountNum)
+		{
+			for(auto &x:currentAccounts)
+			{
+				if(x.getAccountNum() == accountNum)
+				{
+					return &x;
+				}
+			}
+			return NULL;
+		}
 };
 
 int main()
 {
 	BankManagement bank;
 	int accountNumber = 800847;
-	int choice, op=0;
-
-
-	do{
+	int choice;
+	char op=0;
+	do
+	{
 		cout << endl << "WELCOME TO THE BANK OF ATLANTIC OCEAN" << endl << endl;
 		cout << "HOW MAY I HELP YOU TODAY ?" << endl;
 		cout << "1. CREATE A NEW ACCOUNT" << endl;
@@ -249,7 +306,20 @@ int main()
 					cout << "YOU NEED A MINIMUM OF 1000 TO OPEN A BANK ACCOUNT!";
 					goto balanceCheck;
 				}
-				bank.AddAccount(firstName, lastName, age, phone, address, adhaar, accountNumber++, balance - 0.1);
+				char acc;
+				type : 
+				cout << "SELECT ACCOUNT TYPE : SAVING(S) or CURRENT(C) : ";
+				cin >> acc;
+				if(acc == 'S' || acc=='s')
+					bank.AddSavingAccount(firstName, lastName, age, phone, address, adhaar, accountNumber++, balance - 0.1);
+				else if(acc == 'c' || acc == 'C')
+					bank.AddCurrentAccount(firstName, lastName, age, phone, address, adhaar, accountNumber++, balance - 0.1, 0.2);
+				else
+				{
+					cout << "INVALID KEY!" << endl;
+					goto type;
+				}
+				break;
 			}
 			case 2:
 			{
@@ -258,13 +328,120 @@ int main()
 				cout << "2. WITHDRAWAL" << endl;
 				cout << "3. APPLY FOR A LOAN" << endl;
 				cout << "4. PAY LOAN INSTALLMENT" << endl;
+				cout << "5. ADD INTREST TO THE ACCOUNT" << endl;
 				int operation;
 				cin >> operation;
 				switch(operation)
 				{
 					case 1:
-					break;
+					{
+						int accountNum;
+						double amount;
+						cout << "ENTER ACCOUNT NUMBER : ";
+						cin >> accountNum;
+						SavingAccount* account = bank.findSavingAccount(accountNum);
+						currentAccount* account_ = bank.findCurrentAccount(accountNum);
+						if(account != NULL)
+						{
+							fflush(stdin);
+							cout << "ENTER AMOUNT : ";
+							cin >> amount;
+							account->deposit(amount);
+						}
+						else if(account_ != NULL)
+						{
+							fflush(stdin);
+							cout << "ENTER AMOUNT : ";
+							cin >> amount;
+							account->deposit(amount);
+						}
+						else
+						{
+							cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
+						}
+						break;
+					}
+					case 2:
+					{
+						int accountNum;
+						double amount;
+						cout << "ENTER ACCOUNT NUMBER : ";
+						cin >> accountNum;
+						SavingAccount* account = bank.findSavingAccount(accountNum);
+						currentAccount* account_ = bank.findCurrentAccount(accountNum);
+						if(account != NULL)
+						{
+							fflush(stdin);
+							cout << "ENTER AMOUNT : ";
+							cin >> amount;
+							account->withdraw(amount);
+						}
+						else if(account_ != NULL)
+						{
+							fflush(stdin);
+							cout << "ENTER AMOUNT : ";
+							cin >> amount;
+							account->withdraw(amount);
+						}
+						else
+						{
+							cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
+						}
+						break;
+					}
+					case 3:
+					{
+						int accountNum;
+						double amount;
+						cout << "ENTER ACCOUNT NUMBER : ";
+						cin >> accountNum;
+						currentAccount* account = bank.findCurrentAccount(accountNum);
+						if(account != NULL)
+						{
+							fflush(stdin);
+							cout << "ENTER AMOUNT : ";
+							cin >> amount;
+							account->loan(amount);
+						}
+						else
+						{
+							cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
+						}
+						break;
+					}
+					case 4:
+					{
+						int accountNum;
+						double amount;
+						cout << "ENTER ACCOUNT NUMBER : ";
+						cin >> accountNum;
+						currentAccount* account = bank.findCurrentAccount(accountNum);
+						if(account != NULL)
+						{
+							fflush(stdin);
+							cout << "ENTER AMOUNT : ";
+							cin >> amount;
+							account->payInstallment(amount);
+						}
+						else
+						{
+							cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
+						}
+						break;
+					}
+					case 5:
+					{
+						int accountNum;
+						double amount;
+						cout << "ENTER ACCOUNT NUMBER : ";
+						cin >> accountNum;
+						currentAccount* account = bank.findCurrentAccount(accountNum);
+						if(account != NULL) account->addIntrest();
+						else cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
+						break;
+					}
 				}
+				break;
 			}
 			case 3:
 			{
@@ -273,62 +450,48 @@ int main()
 			}
 			case 4:
 			{
+				char acc;
+				here : 
+				cout << "SELECT ACCOUNT TYPE : SAVING(S) or CURRENT(C) : ";
+				cin >> acc;
 				int accountNum;
 				cout << "ENTER ACCOUNT NUMBER : ";
 				cin >> accountNum;
-				bank.searchSavingAccount(accountNum);
-				break;
-
-				// int accountNum;
-				// double amount;
-				// cout << "ENTER ACCOUNT NUMBER : ";
-				// cin >> accountNum;
-				// SavingAccount* account = bank.findSavingAccount(accountNum);
-				// if(account != NULL)
-				// {
-				// 	fflush(stdin);
-				// 	cout << "ENTER AMOUNT : ";
-				// 	cin >> amount;
-				// 	account->deposit(amount);
-				// }
-				// else
-				// {
-				// 	cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
-				// }
-				// break;
-			}
-			case 5:{
-				int accountNum;
-				double amount;
-				cout << "ENTER ACCOUNT NUMBER : ";
-				cin >> accountNum;
-				SavingAccount* account = bank.findSavingAccount(accountNum);
-				if(account != NULL)
-				{
-					fflush(stdin);
-					cout << "ENTER AMOUNT : ";
-					cin >> amount;
-					account->withdraw(amount);
-				}
+				if(acc == 'S' || acc=='s')
+					bank.searchSavingAccount(accountNum);
+				else if(acc == 'c' || acc == 'C')
+					bank.searchCurrentAccount(accountNum);
 				else
 				{
-					cout << endl << "\x1B[91m ACCOUNT DOSEN'T EXIST! \033[0m" << endl << endl;
+					cout << "INVALID KEY!" << endl;
+					goto here;
 				}
 				break;
 			}
-			case 6:{
+			case 5:
+			{
+			 	ofstream database;
+				database.open("Bank.txt", ios::app);
+				database.write((char*)&bank,sizeof(bank));
+				database.close();
+				cout << "ADIOS, SEE YOU AGAIN!" << endl;
 				exit(1);
 				break;
 			}
-			system("cls");
+		}
+		cout << "DO YOU WISH TO CONTINUE? [Y/N] : ";
+		cin >> op;
+		if(op == 'N' || op == 'n')
+		{
+			ofstream database;
+			database.open("Bank.txt", ios::app);
+			database.write((char*)&bank,sizeof(bank));
+			database.close();
+			cout << "ADIOS, SEE YOU AGAIN!" << endl;
+			exit(1);
+		}
+		system("cls");
 	}
-	cout << "DO YOU WISH TO CONTINUE? [Y/N] : ";
-	cin >> op;
-	if(op == 'N' || op == 'n')
-	{
-		cout << "ADIOS, SEE YOU AGAIN!" << endl;
-		exit(1);
-	}
-	}while(op == 'y'||op =='Y');
+	while(op == 'y'||op =='Y');
 	return 0;
 }
